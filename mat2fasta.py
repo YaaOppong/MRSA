3ab64c523e71fa95fcd2e438bf0162d742196281
YaaOppong <yaaoppong@hotmail.com>
2018-10-03 14:01:53 +0100

    hashed out non-working functions

--- mat2fasta.py
+++ mat2fasta.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import Bio.SeqIO
 import argparse
+import numpy as np
 
 #functiom to read IUPAC matrix and store as data frame############################
 def readmat(mat):
@@ -84,110 +85,126 @@ def unionposmatBIN(mat, reference, additionals_snps):
 	mat=mat.sort_values(by=['POS'])
 	return(mat)
 
+
+
+
+#####BELOW REQUIRES DEBUGGING
 #functions to add snps to iupac and add new IUPAC to matrix as new column##########
-def additionalsIUPAC(mat, additional_snps):
-	for additional_snp in additional_snps:
-		name=additional_snp
-		additional_snp=readsnps(additional_snp)
-		mpos=mat['POS']
-		npos=additional_snp.iloc[:,0]
-		newpos=set(mpos).difference(set(npos))
-		mydict={}
-		for i in  range(len(mat['POS'])):
-			mydict[mat.loc[i, 'POS']]= mat.loc[i, 'REF']
-		newdata=[]
-		for pos in newpos:
-			ref=mydict[pos]
-			newrow=[pos,ref]
-			newdata.append(newrow)
-		newdata=pd.DataFrame(newdata)
-		newdata.columns=['refpos','alt']
-		additional_snp=pd.concat([additional_snp, newdata], axis=0)
-		additional_snp=additional_snp.sort_values(by=['refpos'])
-		newmatcol=pd.DataFrame(additional_snp.iloc[:,1])
-		newmatcol.columns=[name]
-		#check mat['POS']==additional_SNPS['refpos']### STILL TO DO###
-		mat=pd.concat([mat, newmatcol], axis=1)
-	return(mat)
+#def additionalsIUPAC(mat, additional_snps):
+#	for additional_snp in additional_snps:
+#		name=additional_snp
+#		additional_snp=readsnps(additional_snp)
+#		mpos=mat['POS']
+#		npos=additional_snp.iloc[:,0]
+#		newpos=set(mpos).difference(set(npos))
+#		mydict={}
+#		for i in  range(len(mat['POS'])):
+#			mydict[mat.loc[i, 'POS']]= mat.loc[i, 'REF']
+#		newdata=[]
+#		for pos in newpos:
+#			ref=mydict[pos]
+#			newrow=[pos,ref]
+#			newdata.append(newrow)
+#		newdata=pd.DataFrame(newdata)
+#		newdata.columns=['refpos','alt']
+#		additional_snp=pd.concat([additional_snp, newdata], axis=0)
+#		additional_snp=additional_snp.sort_values(by=['refpos'])
+#		newmatcol=pd.DataFrame(additional_snp.iloc[:,1])
+#		newmatcol.columns=[name]
+#		#check mat['POS']==additional_SNPS['refpos']### STILL TO DO###
+#		mat=pd.concat([mat, newmatcol], axis=1)
+#	return(mat)
 
 
 #functions to add additional references to BINARY matrix############################
-def additionalsBIN(mat, additional_snps):
-	for additional_snp in additional_snps:
-		name=additional_snp
-		additional_snp=readsnps(additional_snp)
-		mpos=mat['POS']
-		npos=additional_snp.iloc[:,0]
-		newpos=set(mpos).difference(set(npos))
-		mydict={}
-		for i in  range(len(mat['POS'])):
-			mydict[mat.loc[i, 'POS']]= mat.loc[i, 'REF']
-		newcol=[]
-		for pos in additional_snp.iloc[:,0]:
-			newcol.append(1)
-		additional_snp=pd.concat([additional_snp, pd.DataFrame(newcol)], axis=1)
-		additional_snp.columns=['refpos','alt', 'bin' ]
-		newdata=[]
-		for pos in newpos:
-			ref=mydict[pos]
-			newrow=[pos,ref,0]
-			newdata.append(newrow)
-		newdata=pd.DataFrame(newdata)
-		newdata.columns=['refpos','alt', 'bin' ]
-		additional_snp=pd.concat([additional_snp, newdata], axis=0)
-		additional_snp=additional_snp.sort_values(by=['refpos'])
-		newmatcol=pd.DataFrame(additional_snp.iloc[:,2])
-		newmatcol.columns=[name]
-		#check mat['POS']==additional_SNPS['refpos']### STILL TO DO####
-		mat=pd.concat([mat, newmatcol], axis=1)
-	return(mat)
+#def additionalsBIN(mat, additional_snps):
+#	for additional_snp in additional_snps:
+#		name=additional_snp
+#		additional_snp=readsnps(additional_snp)
+#		mpos=mat['POS']
+#		npos=additional_snp.iloc[:,0]
+#		newpos=set(mpos).difference(set(npos))
+#		mydict={}
+#		for i in  range(len(mat['POS'])):
+#			mydict[mat.loc[i, 'POS']]= mat.loc[i, 'REF']
+#		newcol=[]
+#		for pos in additional_snp.iloc[:,0]:
+#			newcol.append(1)
+#		additional_snp=pd.concat([additional_snp, pd.DataFrame(newcol)], axis=1)
+#		additional_snp.columns=['refpos','alt', 'bin' ]
+#		newdata=[]
+#		for pos in newpos:
+#			ref=mydict[pos]
+#			newrow=[pos,ref,0]
+#			newdata.append(newrow)
+#		newdata=pd.DataFrame(newdata)
+#		newdata.columns=['refpos','alt', 'bin' ]
+#		additional_snp=pd.concat([additional_snp, newdata], axis=0)
+#		additional_snp=additional_snp.sort_values(by=['refpos'])
+#		newmatcol=pd.DataFrame(additional_snp.iloc[:,2])
+#		newmatcol.columns=[name]
+#		#check mat['POS']==additional_SNPS['refpos']### STILL TO DO####
+#		mat=pd.concat([mat, newmatcol], ignore_index=True, axis=1)
+#	return(mat)
 
 
+
+#def additionalsIUPAC(mat, additional_snps):
+#	for additional_snp in additional_snps:
+#		name=additional_snp
+#		additional_snp=readsnps(additional_snp)
+#		for pos in mat.loc['POS']:
+#			newcol.iloc[pos]=additional_snp.iloc[np.where(additional_snp.loc['REFPOS'] == pos, 'ALT')]
+#			except
+#				np.where(additional_snp.loc['REFPOS'] == pos)= NA:
+#					newcol.iloc[pos]=mat.loc[pos, 'REF']
+
+#	return(mat)
 #generate multifasta from IUPAC matrix##############################################
-def matfasta(mat, out):
-	outfasta=open(out, 'w')
-	for col in mat.columns()[3:]:
-		a=mat[col]
-		Bio.SeqIO.write(fasta, outfasta, "fasta")
-	outfasta.close()
+#def matfasta(mat, out):
+#	outfasta=open(out, 'w')
+#	for col in mat.columns()[3:]:
+#		a=mat[col]
+#		Bio.SeqIO.write(fasta, outfasta, "fasta")
+#	outfasta.close()
 
 
 ###################################################################################
 ###arg input##
-parser=argparse.ArgumentParser(description='')
-parser.add_argument('--out_type',help='output type required; f (fasta), b \
-	(matbin), i (matiupac)')
-parser.add_argument('--mat', help="Variant matrix in IUPAC format (with 4 d\
-	escriptor columns;'POS' 'REF' 'AL' 'TYPE')")
-parser.add_argument('--matbin', help="Binary variant matrix (with 4 descriptor \
-	columns;'POS' 'REF' 'AL' 'TYPE')")
-parser.add_argument('--out_prefix', help='Prefix for output files')
-parser.add_argument('--additional_snps', help='List of SNP only nucmer files of \
-	additional genomes generated using show-snps', nargs='+')
-parser.add_argument('--reference', help='Reference fasta file')
-args=parser.parse_args()
+#parser=argparse.ArgumentParser(description='')
+#parser.add_argument('--out_type',help='output type required; f (fasta), b \
+#	(matbin), i (matiupac)')
+#parser.add_argument('--mat', help="Variant matrix in IUPAC format (with 4 d\
+#	escriptor columns;'POS' 'REF' 'AL' 'TYPE')")
+#parser.add_argument('--matbin', help="Binary variant matrix (with 4 descriptor \
+#	columns;'POS' 'REF' 'AL' 'TYPE')")
+#parser.add_argument('--out_prefix', help='Prefix for output files')
+#parser.add_argument('--additional_snps', help='List of SNP only nucmer files of \
+#	additional genomes generated using show-snps', nargs='+')
+#parser.add_argument('--reference', help='Reference fasta file')
+#args=parser.parse_args()
 
 ##pipeline execution###############################################################
-if args.out_type=='i':
-	outmatIUPAC=unionposmatIUPAC(args.mat, args.reference, args.additional_snps)
-	outmat=additionalsIUPAC(outmatIUPAC, additional_snps)
-	a.write(outmat)
-	a.close()
+#if args.out_type=='i':
+#	outmatIUPAC=unionposmatIUPAC(args.mat, args.reference, args.additional_snps)
+#	outmat=additionalsIUPAC(outmatIUPAC, additional_snps)
+#	a.write(outmat)
+#	a.close()m
 
 
-if args.out_type=='b':
-	outmatBIN=unionposmatBIN(args.matbin, args.reference, args.additional_snps)
-	outmat=additionalsBIN(outmatBIN, additional_snps)
-	b=open(args.out_prefix + 'mat.bin', 'w')
-	b.write(outmat)
-	b.close()
+#if args.out_type=='b':
+#	outmatBIN=unionposmatBIN(args.matbin, args.reference, args.additional_snps)
+#	outmat=additionalsBIN(outmatBIN, additional_snps)
+#	b=open(args.out_prefix + 'mat.bin', 'w')
+#	b.write(outmat)
+#	b.close()
 
 
-if args.out_type=='f':
-	outmat=unionposmatIUPAC(args.mat, args.reference, args.additional_snps)
-	a.write(outmat)
-	a.close()
-	matfasta(args.outmat, args.out_prefix + '.fasta')
+#if args.out_type=='f':
+#	outmat=unionposmatIUPAC(args.mat, args.reference, args.additional_snps)
+#	a.write(outmat)
+#	a.close()
+#	matfasta(args.outmat, args.out_prefix + '.fasta')
 
 ##################################################################################
 ##################################################################################
