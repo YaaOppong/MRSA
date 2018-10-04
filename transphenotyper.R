0f0de978df4c087e67105d92d11f63354b597fdd
YaaOppong <yaaoppong@hotmail.com>
2018-10-04 10:26:15 +0100

    first transphenotyper commit

---
+++ transphenotyper.R
@@ -0,0 +1,13 @@
+#no explicit evolutionary model
+#no model of wothin host or macro evolution purely a distance based metric- incoprorates natural selection?
+
+
+input: mat, distance cutoff, cluster size cutoff if binary
+
+getDistance()
+
+
+getClusters(distance matrix, distance cutoff)
+
+
+getPhenotype()
