5f779aa5dc7374b4de7b42a336b0589d4b48f2ca
YaaOppong <yaaoppong@hotmail.com>
2018-11-21 12:01:37 +0000

    Update mat2fasta.R

--- mat2fasta.R
+++ mat2fasta.R
@@ -8,6 +8,11 @@ getmat2fastainput<-function(nucmersnps, out_prefix)
 	snps<-read.delim(nucmersnps, header=F)
 	snps2<-as.data.frame(cbind(refpos=as.character(snps[,1]), ref=as.character(snps[,2]), alt=as.character(snps[,3])))
 	#deal with duplicate positions?
+	duplicated<-unique(newisolate$refpos[which(duplicated(newisolate$refpos)==TRUE)])
+	if(length(duplicated)>0)
+	{
+		print(this nucmersnps file contains duplicate positions)	
+	}
 	write.table(snps2, paste(out_prefix, '_refpos_ref_alt.txt', sep=''), quote=F, row.name=F, col.name=T)
 }
 
